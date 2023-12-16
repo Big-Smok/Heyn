723021c8f1d46614bfd52fc24cef0b91f8744ebc
Big-Smok <86967782+Big-Smok@users.noreply.github.com>
2023-12-16 22:38:14 +0300

    Update ile.py

--- ile.py
+++ ile.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup,ReplyKeyboardMarkup
 from pyrogram.enums import ParseMode
-import requests
+import requests,uvloop
 import os, random, string
 
 
@@ -27,11 +27,11 @@ def download_file(url, save_path):
 api_id = 12930802
 api_hash = "e28ec8094a5779645abcacef029a785c"
 bot_token = "6565017763:AAGHHrGGxoQEIiINjzwsJh16rOJRN0S6EjQ"
-
+uvloop.install()
 app = Client("my_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
-parse_mode = ParseMode.MARKDOWN
+#app.set_parse_mode(ParseMode.MARKDOWN)
 keyboar = [[InlineKeyboardButton("PROGRAMMER", url="https://t.me/dev_fury")]]
-buttons = keyboar
+buttons = InlineKeyboardMarkup(keyboar)
 
 
 async def custom_progress(
@@ -40,7 +40,7 @@ async def custom_progress(
 ):
   progress_percentage = (current / total) * 100
   if not progress_percentage == 100:
-    progress_message = f"*Uploading: {progress_percentage:.2f}%*"
+    progress_message = f"**Uploading: {progress_percentage:.2f}%**"
     cat_i = open("ID.txt", "r").read()
     ind = str(cat_i).splitlines()[0]
     messi = str(cat_i).splitlines()[1]
@@ -59,13 +59,13 @@ async def custom_progress(
 async def Start(client, mes):
   await app.send_message(
       mes.chat.id,
-      "*Hi,Im Jafar!\nIm Here To Assist with porting your files from The Web to Telegram\nAll you have to do iss sending me the url*",
+      "**Hi,Im Jafar!\nIm Here To Assist with porting your files from The Web to Telegram\nAll you have to do is send /download_file command with the url\n\nExample:/download_file https://My-File.com/file.zip**",
       reply_markup=buttons)
 
 
 @app.on_message(filters.command("download_file"))
 async def send_file(client, mess):
-  await app.send_message(mess.chat.id, "*Wait For The File To Be Downloaded*")
+  await app.send_message(mess.chat.id, "**Wait For The File To Be Downloaded**")
   url = str(mess.text).split("download_file")[1]
   #name = mess.text[int(str(mess.text).rfind("/")) + 1:]
   characters = string.ascii_letters + string.digits
@@ -73,14 +73,14 @@ async def send_file(client, mess):
   name = f"{name}{mess.text[str(mess.text).rfind('.'):]}"
   print(name)
   download_file(url, name)
-  pro = await app.send_message(mess.chat.id, "*Uploading : 0%*")
+  pro = await app.send_message(mess.chat.id, "**Uploading : 0%**")
   with open("ID.txt", "w") as IDo:
     IDo.write(f"{str(mess.chat.id)}\n{pro.id}")
   await app.send_document(mess.chat.id, name, progress=custom_progress)
   await app.send_message(mess.chat.id,
-                         "*File Sent successfuly*",
+                         "**File Sent successfuly**",
                          reply_markup=buttons)
 
 
-
+app.set_parse_mode(ParseMode.MARKDOWN)
 app.run()
